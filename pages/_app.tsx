ea77f174c7033877d7eeb0c4d53115f1783aef81
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-06-24 08:54:35 +0100

    feat: sets up prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import type { AppProps } from "next/app";
-import Head from "next/head";
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
